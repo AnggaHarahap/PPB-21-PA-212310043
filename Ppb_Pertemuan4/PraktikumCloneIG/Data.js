e88d4a332310252ab86a2c6c6695db235adfa38a
Angga P Harahap <114120852+AnggaHarahap@users.noreply.github.com>
2023-04-30 22:35:05 +0700

    Add files via upload

---
+++ Ppb_Pertemuan4/PraktikumCloneIG/Data.js
@@ -0,0 +1,13 @@
+const Users =[
+    { id: 1, name: "Zaki", fullname: "Zaki Aljabbar" ,gender: "M", story: true },
+    { id: 2, name: "Adrian", fullname: "Adrian Adhari", gender: "M", story: true},
+    { id: 3, name: "Hana", fullname: "Hana Yulia Rahmah", gender: "F", story: false},
+    { id: 4, name: "Lukman", fullname: "Lukman Nurhakim", gender: "M", story: true},
+    { id: 5, name: "Cindy", fullname: "Cindy Kevina", gender: "F", story: false }
+];
+
+const UserFeedObj =[
+    {id: 1, name: "Angga P Harahap", post: require('./assets/Rubicon.dekstop.jpg')}
+]
+
+export {Users, UserFeedObj}
---
+++ Ppb_Pertemuan4/components/Dummy.js
@@ -0,0 +1,21 @@
+const Users = [
+    { id: 1, name: "Zaki", fullname: "Zaki Aljabbar" ,gender: "M" },
+    { id: 2, name: "Adrian", fullname: "Adrian Adhari", gender: "M"},
+    { id: 3, name: "Hana", fullname: "Hana Yulia Rahmah", gender: "F"},
+    { id: 4, name: "Lukman", fullname: "Lukman Nurhakim", gender: "M"},
+    { id: 5, name: "Cindy", fullname: "Cindy Kevina", gender: "F" }
+];
+
+const Suggested = [
+    { id: 1, name: "Zaki", fullname: "Zaki Slebew" ,gender: "M" },
+    { id: 2, name: "Fauzan", fullname: "Fauzan rindu", gender: "M"},
+    { id: 3, name: "Fathur", fullname: "Fathur Tajur", gender: "M"},
+    { id: 4, name: "Fawaz", fullname: "Bawaz Ajja", gender: "M"},
+    { id: 5, name: "Adrian", fullname: "Adrian nylebew", gender: "M" } 
+];
+
+const Followers = [
+    { id: 1, name: "Rizky", fullname: "Rizky Gajelas" ,gender: "M" },
+    { id: 2, name: "Ilham", fullname: "Ilham udin", gender: "M"}
+]
+export {Users, Suggested, Followers};
